0bdbebf196af27b35a97df6008722a98c5b11a3d
JMS2KOR <JMS2KOR@BANM-C-0000T.kor.apac.bosch.com>
2021-02-22 23:49:17 +0530

    first commit

---
+++ src/main/java/com/src/in/Swagger/Project/SwaggerProjectApplication.java
@@ -0,0 +1,14 @@
+package com.src.in.Swagger.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		SpringApplication.run(SwaggerProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/src/in/Swagger/Project/SwaggerProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.src.in.Swagger.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
